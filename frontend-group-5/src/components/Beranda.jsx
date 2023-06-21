4dcb53182e753eb90f2a8629eee99873b02de04a
Dhikaadn <andhikadianpratama057@gmail.com>
2023-06-21 16:08:16 +0700

    finished beranda page

--- frontend-group-5/src/components/Beranda.jsx
+++ frontend-group-5/src/components/Beranda.jsx
@@ -2,22 +2,45 @@ import React from "react";
 import { NavbarBeranda } from "./NavbarBeranda";
 import Button from "react-bootstrap/Button";
 import { BiArrowBack } from "react-icons/bi";
+import {Link} from "react-router-dom";
 import { LoadingBeranda } from "./LoadingBeranda";
 import { TiketFinished } from "./TiketFinished";
 import { NoneSearch } from "./NoneSearch";
 import { ReadyTicket } from "./ReadyTicket";
+import { useDispatch, useSelector } from "react-redux";
+import { getListPenerbangan } from '../redux/actions/search';
+import { Penumpang } from "../redux/actions/penumpang";
+import { useEffect } from 'react';
 export const Beranda = () => {
+  // dispatch -> to change the global state in redux
+  const dispatch = useDispatch();
+
+  // useSelector -> to access the global state (redux)
+  const { listPenerbangan } = useSelector((state) => state.search);
+  const { penumpang } = useSelector((state) => state.penumpang);
+  useEffect(() => {
+    dispatch(getListPenerbangan(),Penumpang());
+  }, [dispatch]);
+  console.log(listPenerbangan)
+  console.log(penumpang)
+  const countPenumpang = penumpang.anak+penumpang.dewasa+penumpang.bayi;
+  console.log(countPenumpang)
   return (
     <div className="Beranda">
       <NavbarBeranda />
       <div className="container-detail">
         <h2 className="title-pilih mb-4">Pilih Penerbangan</h2>
         <div className="container-hari-top">
+        {listPenerbangan?.length > 0 &&
+          listPenerbangan.map((list,i) => (
           <Button className="bt-detail me-3">
             <BiArrowBack className="me-3" />
-            JKT {">"} MLB - 2 Penumpang - Economy
+            {list.departureCity} {">"} {list.arrivalCity} - {countPenumpang} Penumpang - {list.airlineClass}
           </Button>
-          <Button className="bt-pencarian">Ubah Pencarian</Button>
+          ))}
+            
+          
+          <Link to="/"><Button className="bt-pencarian">Ubah Pencarian</Button></Link>
         </div>
         <div className="container-hari-center">
           <div className="hari">
@@ -78,7 +101,7 @@ export const Beranda = () => {
           </div>
         </div>
       </div>
-      <ReadyTicket />
+      {listPenerbangan.length>0?<ReadyTicket />:<NoneSearch/>}
     </div>
   );
 };
--- frontend-group-5/src/components/ReadyTicket.jsx
+++ frontend-group-5/src/components/ReadyTicket.jsx
@@ -4,7 +4,7 @@ import maskapai from '../img/maskapai.jpg';
 import time from '../img/time.jpg';
 import Button from 'react-bootstrap/Button';
 import { useDispatch, useSelector } from "react-redux";
-import { getPosts } from "../redux/actions/post";
+import { getListPenerbangan } from '../redux/actions/search';
 import { useEffect } from 'react';
 
 export const ReadyTicket = () => {
@@ -12,94 +12,91 @@ export const ReadyTicket = () => {
   const dispatch = useDispatch();
 
   // useSelector -> to access the global state (redux)
-  const { posts } = useSelector((state) => state.post);
+  const { listPenerbangan } = useSelector((state) => state.search);
   useEffect(() => {
-    dispatch(getPosts());
+    dispatch(getListPenerbangan());
   }, [dispatch]);
-  console.log(posts)
+  console.log(listPenerbangan)
   return (
     <div className='ReadyTicket'>
         <Accordion defaultActiveKey="0" className='accordion'>
-      <Accordion.Item eventKey="0" className='accordion-item'>
-        <Accordion.Header className='accordion-header'>
-            <div className='container-kelas-pojok-kiri'>
-                <img className='me-3' src={maskapai} alt="logo maskapai"/>
-                Jet Air - Economy
-            </div>
-        </Accordion.Header>
-        <div className='container-detail-jadwal'>
-            <div className='container-jam-kota me-3'>
-                <p className='jam mb-0'>08:00</p>
-                <p className='kota'>JKT</p>
-            </div>
-            <div className='container-garis'>
-                <div className='garis-top'>
-                    4h 0m
+        {listPenerbangan?.length > 0 &&
+          listPenerbangan.map((list,i) => (
+            <Accordion.Item eventKey={i} className='accordion-item' key={i}>
+            <Accordion.Header className='accordion-header'>
+                <div className='container-kelas-pojok-kiri'>
+                    <img className='me-3' src={maskapai} alt="logo maskapai"/>
+                    {list.airlineName} - {list.airlineClass}
                 </div>
-                <div className='garis-bottom'>
-                    Direct
+            </Accordion.Header>
+            <div className='container-detail-jadwal'>
+                <div className='container-jam-kota me-3'>
+                    <p className='jam mb-0'>{list.departureTime}</p>
+                    <p className='kota'>{list.departureCity}</p>
+                </div>
+                <div className='container-garis'>
+                    <div className='garis-top'>
+                        4h 0m
+                    </div>
+                    <div className='garis-bottom'>
+                        Direct
+                    </div>
+                </div>
+                <div className='container-jam-kota-2 ms-3'>
+                    <p className='jam mb-0'>{list.arrivalTime}</p>
+                    <p className='kota'>{list.arrivalCity}</p>
+                </div>
+                <img src={time} width="20" height="20" className='ms-4 mt-3' alt='time-logo'/>
+                <div className='container-harga'>
+                    <p className='harga'>{list.airlinePrice}</p>
+                    <Button className='bt-pilih'>
+                        Pilih
+                    </Button>
                 </div>
             </div>
-            <div className='container-jam-kota-2 ms-3'>
-                <p className='jam mb-0'>08:00</p>
-                <p className='kota'>JKT</p>
-            </div>
-            <img src={time} width="20" height="20" className='ms-4 mt-3' alt='time-logo'/>
-            <div className='container-harga'>
-                <p className='harga'>IDR 4.950.000</p>
-                <Button className='bt-pilih'>
-                    Pilih
-                </Button>
-            </div>
-        </div>
-        <Accordion.Body>
-          <div className='container-keberangkatan'>
-            <div className='container-keberangkatan-left'>
-            {posts?.length > 0 &&
-          posts.map((post,i) => (
-              <div key={i}>
-                <p>{post.kotaKeberangkatan}</p>
-                <p>{post.schedulesList[0].waktuKeberangkatan}</p>
+            <Accordion.Body>
+              <div className='container-keberangkatan'>
+                <div className='container-keberangkatan-left'>
+                  <p className='title-detail'>Detail Penerbangan</p>
+                  <p className='jam'>{list.departureTime}</p>
+                  <p>{list.departureDate}</p>
+                  <p>{list.departureCity}</p>
+                </div>
+                <div className='container-keberangkatan-right'>
+                  <p className='title-status'>
+                    Keberangkatan
+                  </p>
+                </div>
               </div>
+              <div className='container-informasi ps-5'>
+                <div className='container-informasi-top'>
+                  <p className='maskapai-bold'>{list.airlineName}-{list.airlineClass}-{list.airlineCode}</p>
+                </div>
+                <div className='container-informasi-logo'>
+                  <img src={maskapai} alt="maskapai logo"/>
+                  <p className='informasi-bold'>Informasi:</p>
+                </div>
+                <p>Baggage {list.checkedBaggage} kg</p>
+                <p>Cabin baggage {list.cabinBaggage} kg</p>
+                <p>In Flight Entertainment</p>  
+              </div>
+              <div className='container-kedatangan'>
+                <div className='container-kedatangan-left'>
+                  <p className='jam'>{list.arrivalTime}</p>
+                  <p>{list.arrivalDate}</p>
+                  <p>{list.arrivalCity}</p>
+                </div>
+                <div className='container-kedatangan-right'>
+                  <p className='title-status'>
+                    Kedatangan
+                  </p>
+                </div>
+              </div>
+            </Accordion.Body>
+          </Accordion.Item>
             ))}
-              <p className='title-detail'>Detail Penerbangan</p>
-              <p className='jam'>07.00</p>
-              <p>3 Maret 2023</p>
-              <p>Soekarno Hatta - Terminal 1A Domestik</p>
-            </div>
-            <div className='container-keberangkatan-right'>
-              <p className='title-status'>
-                Keberangkatan
-              </p>
-            </div>
-          </div>
-          <div className='container-informasi ps-5'>
-            <div className='container-informasi-top'>
-              <p className='maskapai-bold'>Jet Air-Economy-JT-203</p>
-            </div>
-            <div className='container-informasi-logo'>
-              <img src={maskapai} alt="maskapai logo"/>
-              <p className='informasi-bold'>Informasi:</p>
-            </div>
-            <p>Baggage 20 kg</p>
-            <p>Cabin baggage 7 kg</p>
-            <p>In Flight Entertainment</p>  
-          </div>
-          <div className='container-kedatangan'>
-            <div className='container-kedatangan-left'>
-              <p className='jam'>11.00</p>
-              <p>3 Maret 2023</p>
-              <p>Melbourne International Airport</p>
-            </div>
-            <div className='container-kedatangan-right'>
-              <p className='title-status'>
-                Kedatangan
-              </p>
-            </div>
-          </div>
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="1" className='accordion-item'>
+      
+      {/* <Accordion.Item eventKey="1" className='accordion-item'>
         <Accordion.Header className='accordion-header'>
             <div className='container-kelas-pojok-kiri'>
                 <img className='me-3' src={maskapai} alt="logo maskapai"/>
@@ -170,7 +167,7 @@ export const ReadyTicket = () => {
             </div>
           </div>
         </Accordion.Body>
-      </Accordion.Item>
+      </Accordion.Item> */}
     </Accordion>
     </div>
     
